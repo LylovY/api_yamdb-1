5ac89bb8504ee6684c2c3a9dd7e1fed9f45d2e1f
AlexanderZug <alexandertsygankov.unterwegs@gmail.com>
2022-08-11 15:37:40 +0300

    исправил ошибки в апи для тайтла, жанра и категории

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -8,7 +8,7 @@ class IsAdminOrSuperuser(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return request.user.is_authenticated and (
-            request.user.role == 'admin' or request.user.is_superuser
+                request.user.role == 'admin' or request.user.is_superuser
         )
 
 
@@ -20,6 +20,14 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return (
-            request.method in permissions.SAFE_METHODS
-            or obj.author == request.user
+                request.method in permissions.SAFE_METHODS
+                or obj.author == request.user
+        )
+
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return (
+                request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated and request.user.role == 'admin'
         )
